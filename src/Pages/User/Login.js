fe7921db0966f0e43a5054c64852455184c5f92b
sajjad-mugdho <sajjadmugdho@yahoo.com>
2022-11-14 16:57:19 +0600

    fixed login problem

--- src/Pages/User/Login.js
+++ src/Pages/User/Login.js
@@ -17,6 +17,7 @@ const Login = () => {
 
     const handleLogin = (e) => {
         e.preventDefault();
+        
         const form = e.target;
         const email = form.email.value;
         const password = form.password.value;
@@ -60,7 +61,7 @@ const Login = () => {
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm  py-20 shadow-2xl bg-base-100">
                     <h1 className="text-5xl text-center font-bold">Login</h1>
-                    <form onClick={handleLogin} className="card-body">
+                    <form onSubmit={handleLogin} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Email</span>
